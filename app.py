916f022cc8118865bbb528f5e97b7d493da9c816
KRUPESH_PRAJAPATI <119573118+Krupeshprajapati@users.noreply.github.com>
2026-01-22 14:56:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,13 @@ if __name__ == "__main__":
     init_db()
     app.run(debug=True)
 
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
+
+# import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
