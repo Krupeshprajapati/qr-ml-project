93c14f0e384a84a69cc84adaf2a48e1879c6f12d
KRUPESH_PRAJAPATI <119573118+Krupeshprajapati@users.noreply.github.com>
2026-01-22 15:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def show_data():
 if __name__ == "__main__":
     init_db()
     # app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 # if __name__ == "__main__":
@@ -69,5 +69,6 @@ if __name__ == "__main__":
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 8080))
 #     app.run(host="0.0.0.0", port=port)
+
 
 
