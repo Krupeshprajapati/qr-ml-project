6b2918ebeab5b7e9e02a1adaf7cc173a6db57be7
KRUPESH_PRAJAPATI <119573118+Krupeshprajapati@users.noreply.github.com>
2026-01-22 14:22:00 +0530

    Add files via upload

---
+++ generate_qr.py
@@ -0,0 +1,15 @@
+import qrcode
+
+# url = "http://192.168.1.71:5000/scan"
+# url = "https://abcd-xyz.ngrok-free.app/scan"
+# url = "https://yourapp.onrender.com/scan"
+
+# url = "https://qr-ml-project.onrender.com/scan"
+# url = "https://qr-ml-projectt.vercel.app/scan"
+url = "https://qr-ml-projectt.vercel.app/scan"
+
+
+img = qrcode.make(url)
+img.save("qr_code.png")
+
+print("QR Code Created")
